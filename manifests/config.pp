f7f22f7e6a540089ce907a8cfe907e893c902bb4
Samuli Seppänen <samuli@openvpn.net>
2015-11-13 14:27:50 +0000

    Major restructuring and improvements
    - Integrate webserver (nginx) management
    - Integrate packet filtering management using ::webserver
    - Add GPG key management using golja-gnupg
    - Add parameter validation using puppetlabs/stdlib
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,11 +6,20 @@
 class freight::config
 (
     $varcache,
+    $gpg_key_id,
     $gpg_key_email,
-    $gpg_key_passphrase
+    $gpg_key_passphrase,
+    $manage_webserver
 
 ) inherits freight::params
 {
+
+    validate_string($gpg_key_id)
+    validate_string($gpg_key_email)
+
+    class { '::freight::config::gnupg':
+        gpg_key_id => $gpg_key_id,
+    }
 
     if $gpg_key_passphrase {
         $gpg_key_passphrase_line = 'GPG_PASSPHRASE_FILE="/etc/freight.pass"'
@@ -18,13 +27,26 @@ class freight::config
         $gpg_key_passphrase_line = '#GPG_PASSPHRASE_FILE="/etc/freight.pass"'
     }
 
+    # Set default values
+    File {
+        owner   => $::os::params::adminuser,
+        group   => $::os::params::admingroup,
+    }
+
+    # Check whether we're told to manage the webserver and set a require 
+    # accordingly
+    $require = $manage_webserver ? {
+        true    => Nginx::Http_server['freight'],
+        false   => undef,
+        default => undef,
+    }
+
     # The directory served by the webserver
     file { 'freight-varcache-dir':
-        ensure => directory,
-        name   => $varcache,
-        owner  => $::os::params::adminuser,
-        group  => $::os::params::admingroup,
-        mode   => '0755',
+        ensure  => directory,
+        name    => $varcache,
+        mode    => '0755',
+        require => $require,
     }
 
     # Freight configuration file
@@ -32,8 +54,6 @@ class freight::config
         ensure  => present,
         name    => '/etc/freight.conf',
         content => template('freight/freight.conf.erb'),
-        owner   => $::os::params::adminuser,
-        group   => $::os::params::admingroup,
         mode    => '0644',
         require => Class['freight::install'],
     }
@@ -46,8 +66,6 @@ class freight::config
             ensure  => present,
             name    => '/etc/freight.pass',
             content => template('freight/freight.pass.erb'),
-            owner   => $::os::params::adminuser,
-            group   => $::os::params::admingroup,
             mode    => '0600',
             require => Class['freight::install'],
         }
--- manifests/init.pp
+++ manifests/init.pp
@@ -6,11 +6,36 @@
 # == Parameters
 #
 # [*manage*]
-#  Whether to manage freight with Puppet or not. Valid values are 'yes' 
-#  (default) and 'no'.
+#   Whether to manage freight with Puppet or not. Valid values are true 
+#   (default) and false.
+# [*manage_webserver*]
+#   Whether to manage the webserver (nginx) or not. Valid values are true 
+#   (default) and false. If you set this to false, make sure that a webserver is 
+#   configured and that the parent directory of $varcache directory exists 
+#   before this class is included.
+# [*allow_address_ipv4*]
+#   IPv4 addresses/networks from which to allow connections. This parameter can
+#   be either a string or an array. Defaults to 'anyv4' which means that access
+#   is allowed from any IPv4 address. Uses the webserver module to do the hard
+#   lifting. This parameter has no effect if $manage_webserver is false.
+# [*allow_address_ipv6*]
+#   As above but for IPv6 addresses. Defaults to 'anyv6', thus allowing access 
+#   from any IPv6 address. This parameter has no effect if $manage_webserver is 
+#   false.
+# [*document_root*]
+#   The document root for the webserver. This parameter is ignored unless you've 
+#   set $manage_webserver to true. Defaults to '/var/www'.
 # [*varcache*]
-#   The directory where freight-managed repositories are placed. For example 
-#   '/var/www/repos'. Make sure the parent directory exists. No default value.
+#   The directory where freight-managed repositories are placed. The default 
+#   value is '/var/www/apt'.
+# [*gpg_key_id*]
+#   The ID of the key to install. For example 'D50582E6'. The private and public 
+#   keys need to be on the Puppet fileserver and accessible at these URIs:
+#   
+#   "puppet:///files/${gpg_key_id}-private.key"
+#   
+#   "puppet:///files/${gpg_key_id}-public.key"
+#
 # [*gpg_key_email*]
 #   Email address of the package signing key - check with "gpg --list-keys". No 
 #   default value.
@@ -22,7 +47,7 @@
 # == Examples
 #
 #   class { 'freight':
-#       varcache => '/var/www/repos',
+#       gpg_key_id => 'D50582E6',
 #       gpg_key_email => 'packager@domain.com',
 #       gpg_key_passphrase => 'mysecretpassphrase',
 #   }
@@ -31,29 +56,49 @@
 #
 # Samuli Seppänen <samuli.seppanen@gmail.com>
 #
+# Samuli Seppänen <samuli@openvpn.net>
+#
 # == License
 #
 # BSD-license. See file LICENSE for details.
 #
 class freight
 (
-    $varcache,
+    $gpg_key_id,
     $gpg_key_email,
+    $allow_address_ipv4 = 'anyv4',
+    $allow_address_ipv6 = 'anyv6',
+    $document_root = '/var/www',
+    $varcache = '/var/www/apt',
     $gpg_key_passphrase=undef,
-    $manage = 'yes'
+    $manage = true,
+    $manage_webserver = true
 )
 {
 
-if $manage == 'yes' {
+# Validate parameters
+validate_bool($manage)
+validate_bool($manage_webserver)
 
+if $manage {
+
+    if $manage_webserver {
+        class { '::freight::webserver':
+            document_root      => $document_root,
+            allow_address_ipv4 => $allow_address_ipv4,
+            allow_address_ipv6 => $allow_address_ipv6,
+        }
+    }
     include ::freight::aptrepo
-
     include ::freight::install
 
     class { '::freight::config':
         varcache           => $varcache,
+        gpg_key_id         => $gpg_key_id,
         gpg_key_email      => $gpg_key_email,
         gpg_key_passphrase => $gpg_key_passphrase,
+        # We need to pass this parameter to set the value of a require
+        manage_webserver   => $manage_webserver,
     }
 }
 }
